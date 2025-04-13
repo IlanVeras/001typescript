3ef9c8936b0b87abd922677505f45bcdd4619da9
Ilan Gustav Veras <ilanbarrosveras3@gmail.com>
2025-04-13 18:04:34 -0400

    aula 104

---
+++ 104/script.js
@@ -0,0 +1,7 @@
+"use strict";
+const n = 1;
+const n2 = 10;
+function somar(a, b) {
+    return a + b;
+}
+console.log(somar(5, 6));
---
+++ 104/script.ts
@@ -0,0 +1,6 @@
+const n = 1
+const n2 = 10
+function somar(a: number, b: number) {
+    return a + b;
+}
+console.log(somar(5, 6));
